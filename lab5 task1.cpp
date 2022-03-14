db3ecd97e470bc1ddbc1d1eb0a6ad3875ab6c62b
Mujtabakhakwani <101019352+Mujtabakhakwani@users.noreply.github.com>
2022-03-14 23:57:43 +0500

    Create lab5 task1.cpp
    task 1 for CS112Lab5

---
+++ lab5 task1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class heater {
+private:
+	int temp;
+public:
+	heater() :temp(15) {
+		cout << "temp is  " << temp << endl;
+	}
+		//int temp = 15;
+		int warmer() {
+			temp = temp + 5;
+			return temp;
+		}
+		int cooler() {
+			temp = temp - 5;
+			return temp;
+	}
+};
+int main() {
+	heater h1;
+	h1.warmer();
+	h1.cooler();
+	int option;
+	while (true) {
+		cout << "press option 1 for warmer and 2 for cooler" << endl;
+		cin >> option;
+		switch (option) {
+		case 1:
+			cout <<"new temp is " << h1.warmer() << endl;
+			break;
+		case 2:
+			cout <<"new temp is " << h1.cooler() << endl;
+			break;
+		}
+	}
+}
